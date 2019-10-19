4ef6314de8c32c6f2ae1f88451999861c61353df
Nguyen Dao <nguyen.dao@christinas.vn>
2019-10-19 17:00:46 +0700

    asdf

--- naymam/settings.py
+++ naymam/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1w0aqt&dg3v#pn04blpo($tn$(g2p5c!urv()e)l*9inwxw*%2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
