b45097a10988188bb7e9f91777354a93fb516dc4
Nguyen Dao <nguyen.dao@christinas.vn>
2019-11-20 12:08:11 +0700

    add postgres

--- naymam/settings.py
+++ naymam/settings.py
@@ -77,10 +77,16 @@ WSGI_APPLICATION = 'naymam.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'naymam',
+        'USER': 'naymam',
+        'PASSWORD': 'staylabxnaymam',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
