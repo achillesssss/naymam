5f78cc561cc363894317816f9e74cdcf0268cd1e
Nguyen Dao <nguyen.dao@christinas.vn>
2019-11-20 10:19:52 +0700

    allow naymam.co

--- naymam/settings.py
+++ naymam/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1w0aqt&dg3v#pn04blpo($tn$(g2p5c!urv()e)l*9inwxw*%2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '.naymam.co']
 
 
 # Application definition
